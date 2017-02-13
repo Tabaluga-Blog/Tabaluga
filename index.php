2c2232bf70a906257472eeb148c74d9033356804
Bloodlister <AsenJ.Mihaylov@gmail.com>
2017-02-13 23:53:38 +0200

    First Commit

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Home</title>
+        <style media="screen">
+            body {
+                display: flex;
+                flex-direction: column;
+                justify-content: center;
+                min-height: 100vh;
+                padding: 0;
+                margin:0;
+            }
+            h1 {
+                display: block;
+                font-family: "Arial";
+                text-align: center;
+            }
+        </style>
+    </head>
+    <body>
+        <h1>Let's make something awesome!</h1>
+    </body>
+</html>
