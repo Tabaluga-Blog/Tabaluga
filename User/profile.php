07958a45d1ce775a0759d9df4364e29913919680
Gabriela <gabi.ivanova@gmail.com>
2017-03-15 16:57:54 +0200

    get selected user msgs

--- User/profile.php
+++ User/profile.php
@@ -11,33 +11,19 @@ require_once '../Access/notLogged.php';
 require_once("../header.php");
 ?>
 
-    <!DOCTYPE html>
-    <html>
-    <head>
-        <meta charset="UTF-8">
-        <title></title>
-        <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet"
-              integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN"
-              crossorigin="anonymous">
-        <link rel="stylesheet" href="styles/main.css">
-        <link rel="stylesheet" href="styles/header.css">
-        <link rel="stylesheet" href="styles/sidebar.css">
-    </head>
-<body>
-
-<!-- ===================================================== -->
 
 <h1 class="text-center">
-    Your Posts
+    Posts
 </h1>
 
 <div class="post-container">
-    <!-- $post should be changed to an object of type Post -->
     <?php
+
+    $id = $_GET['id'];
 
     $postService = new PostService();
 
-    $allPosts = $postService->getPostsByUserId($_SESSION['user']->getId());
+    $allPosts = $postService->getPostsByUserId($id);
 
     require_once "../views/showPosts.view.php"
     ?>
