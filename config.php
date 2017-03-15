ad8e4863d94b2e15d749550152406cda74728644
Gabriela <gabi.ivanova@gmail.com>
2017-03-15 19:55:56 +0200

    mail system added

---
+++ config.php
@@ -0,0 +1,17 @@
+<?php
+//db settings
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'tabalugadb');
+
+
+//mail settings
+define('MAIL_HOST', "smtp.gmail.com");
+define('MAIL_PORT', "587");
+define('MAIL_AUTH', "true");
+define('MAIL_SECURE', "tls");
+define('MAIL_USERNAME', "tabalugaweb");
+define('MAIL_PASSWORD', "softuniphp");
+define('MAIL_FROM', "Tabaluga WebBlog");
+define('MAIL_FROMNAME', "Tabaluga WebBlog");
