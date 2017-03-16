75ae7b2f4b709a40c78f439bf7c21b580cb6cc3d
Rumen Panchev <rpanchev9708@gmail.com>
2017-03-16 16:15:53 +0200

    Remove comment

--- Post/create.php
+++ Post/create.php
@@ -1,6 +1,6 @@
 <?php
 
-// use Services\PostServices\PostService;
+use Services\PostServices\PostService;
 use Services\PostServices\MakePostService;
 
 require_once '../Data/User.php';
@@ -9,7 +9,7 @@ require_once '../Services/PostServices/PostService.php';
 require_once '../Access/notLogged.php';
 require_once '../header.php';
 
-//$ps = new PostService();
+$ps = new PostService();
 $message = "";
 
 if (isset($_POST['submit'])) {
